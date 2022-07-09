20be7cc685a43a624ce794a0a2e8902cca0d392a
Henry-Azer <henryazer@outlook.com>
2022-07-09 19:22:40 +0200

    implement basics

---
+++ lib/answer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text(answerText),
+        onPressed: selectHandler,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.lightGreen,
+        ),
+        // Color: Colors.blue,
+        // textColor: Colors.white,
+      ),
+    );
+  }
+}
